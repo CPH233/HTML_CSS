3f776d45fe76ba5235305c0bdcca8daeb7a4eb43
林青 谭 <974043826@qq.com>
2019-12-12 17:31:53 +0800

    添加项目文件。

---
+++ HTML_CSS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HTML_CSS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
